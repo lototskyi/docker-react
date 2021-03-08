f14e5d5e745e284615d9a4d7ab67ec5447684326
alex <o.lototskyi@gmail.com>
2021-03-08 10:50:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. I was changed!
         </p>
         <a
           className="App-link"
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+	  <p className="App-intro">
+		Hi there!
+	  </p>
     </div>
   );
 }
